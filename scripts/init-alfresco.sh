8fa57daaee8a1a21abfc7c22bed6809e639be68b
crilcqtech <dave.letourneau@crilcq.ulaval.ca>
2018-03-22 14:59:20 -0400

    Découplage passwords + base Alfresco

--- scripts/init-alfresco.sh
+++ scripts/init-alfresco.sh
@@ -1,14 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
-MYSQL_ROOT_PASSWORD="00496CD69C0D2572FBBA705FC1D1"
+source ../.env
 
-echo "Initialisation de la base de données"
+echo "Initialisation"
 echo -n "- Création de la base de données 'alfresco'... "
 docker exec crilcq-mysql /usr/bin/mysql -uroot -p$MYSQL_ROOT_PASSWORD -ss -e "CREATE DATABASE alfresco /*\!40100 DEFAULT CHARACTER SET utf8 */;" 2>/dev/null
 echo "Ok"
 
 echo -n "- Création de l'utilisateur 'alfresco' et application des privilèges... "
-docker exec crilcq-mysql /usr/bin/mysql -uroot -p$MYSQL_ROOT_PASSWORD -ss -e "CREATE USER 'alfresco'@'%' IDENTIFIED BY 'PIaVomey6Qqa5EYbUWYGUzV98a';" 2>/dev/null
+docker exec crilcq-mysql /usr/bin/mysql -uroot -p$MYSQL_ROOT_PASSWORD -ss -e "CREATE USER 'alfresco'@'%' IDENTIFIED BY '$MYSQL_ALFRESCO_PASSWORD';" 2>/dev/null
 docker exec crilcq-mysql /usr/bin/mysql -uroot -p$MYSQL_ROOT_PASSWORD -ss -e "GRANT ALL PRIVILEGES ON alfresco.* TO 'alfresco'@'%';" 2>/dev/null
 docker exec crilcq-mysql /usr/bin/mysql -uroot -p$MYSQL_ROOT_PASSWORD -ss -e "FLUSH PRIVILEGES;" 2>/dev/null
 echo "Ok"
@@ -16,11 +16,19 @@ echo "Ok"
 echo -n "- Importation des données... "
 # cat ../data/alfresco/alfresco.sql | docker exec -i crilcq-mysql /usr/bin/mysql -uroot -p$MYSQL_ROOT_PASSWORD alfresco 2>/dev/null
 echo "Ok"
-echo
 
-echo "Initialisation des documents"
-echo -n "- Copie des données (Cette opération va prendre plusieurs minutes)... "
+echo -n "- Copie des fichiers (Cette opération va prendre plusieurs minutes)... "
 # cat ../data/alfresco/alfresco.tar.bz2 | docker exec -i crilcq-website tar Cxjf /alf_data/ -
 echo "Ok"
+
+echo "Finalisation"
+echo -n "- Application du nouveau mot de passe... "
+docker exec crilcq-website /bin/sed -Ei "s/db\.password=.*$/db.password=$MYSQL_ALFRESCO_PASSWORD/g" /var/lib/tomcat7/conf/catalina.properties
+echo "Ok"
+
+echo -n "- Nettoyage/activation... "
+docker exec crilcq-website rm /usr/share/tomcat7/CONFIGURATION_NEEDED
+echo "Ok"
 echo
+
 
--- scripts/init-website.sh
+++ scripts/init-website.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
-MYSQL_ROOT_PASSWORD="00496CD69C0D2572FBBA705FC1D1"
+source ../.env
 
 echo "Initialisation de la base de données"
 echo -n "- Création de la base de données 'typo3'... "
@@ -8,7 +8,7 @@ docker exec crilcq-mysql /usr/bin/mysql -uroot -p$MYSQL_ROOT_PASSWORD -ss -e "CR
 echo "Ok"
 
 echo -n "- Création de l'utilisateur 'typo3' et application des privilèges... "
-docker exec crilcq-mysql /usr/bin/mysql -uroot -p$MYSQL_ROOT_PASSWORD -ss -e "CREATE USER 'typo3'@'%' IDENTIFIED BY 'GNpI4uz22dc3VX6VC';" 2>/dev/null
+docker exec crilcq-mysql /usr/bin/mysql -uroot -p$MYSQL_ROOT_PASSWORD -ss -e "CREATE USER 'typo3'@'%' IDENTIFIED BY '$MYSQL_TYPO3_PASSWORD';" 2>/dev/null
 docker exec crilcq-mysql /usr/bin/mysql -uroot -p$MYSQL_ROOT_PASSWORD -ss -e "GRANT ALL PRIVILEGES ON typo3.* TO 'typo3'@'%';" 2>/dev/null
 docker exec crilcq-mysql /usr/bin/mysql -uroot -p$MYSQL_ROOT_PASSWORD -ss -e "FLUSH PRIVILEGES;" 2>/dev/null
 echo "Ok"
@@ -18,9 +18,14 @@ cat ../data/website/typo3.sql | docker exec -i crilcq-mysql /usr/bin/mysql -uroo
 echo "Ok"
 echo
 
-echo "Initialisation de l'arborescence du site"
+echo "Initialisation du site"
 echo -n "- Copie des données (l'opération peut prendre plusieurs minutes)... "
 cat ../data/website/typo3.tar.bz2 | docker exec -i crilcq-website tar Cxjf /var/www/html/ -
+echo "Ok"
+
+echo -n "- Application des nouveaux mots de passe... "
+docker exec crilcq-website /bin/sed -Ei "s/'password' => '[^']+'/'password' => '$MYSQL_TYPO3_PASSWORD'/g" /var/www/html/crilcq.org/typo3conf/LocalConfiguration.php
+docker exec crilcq-website /bin/sed -Ei "s/'installToolPassword' => '[^']+'/'password' => '$TYPO3_INSTALL_TOOL_PASSWORD'/g" /var/www/html/crilcq.org/typo3conf/LocalConfiguration.php
 echo "Ok"
 echo
 
